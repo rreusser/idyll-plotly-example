c6f204ab56463787efa5770d6aa78b2be5fbbd5f
Ricky Reusser <rsreusser@gmail.com>
2019-11-08 15:54:48 -0800

    Simplify and rebuild

--- components/plot.js
+++ components/plot.js
@@ -1,26 +1,10 @@
 const React = require('react');
+const Plot = (() => {try { return require('react-plotly.js').default } catch (e) {}})();
 
-// Plotly uses `document`, which causes static server-side rendering to fail, so we
-// allow Plotly to be undefined if rendering on the server.
-//
-// Also, I've included the plotly-basic.min.js bundle since it's *significantly* smaller
-// than the basic kitchen-sink build that you get it you require('plotly.js'). You can
-// see more details about the bundles and their sizes here:
-// https://github.com/plotly/plotly.js/tree/master/dist#bundle-information
-var Plotly = (() => {try { return require('plotly.js/dist/plotly-basic.min.js') } catch (e) {}})()
-
-class Plot extends React.Component {
-  getElementReference (graphDiv) {
-    // This method is only called on the client so that the div will be blank if
-    // rendered statically.
-    Plotly.plot(graphDiv, this.props.data)
-  }
-
+class PlotComponent extends React.Component {
   render() {
-    return (
-      <div ref={this.getElementReference.bind(this)}></div>
-    );
+    return Plot ? <Plot {...this.props.data}/> : <div/>;
   }
 }
 
-module.exports = Plot;
+module.exports = PlotComponent;
